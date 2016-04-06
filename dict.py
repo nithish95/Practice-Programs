62c10ae1afd09a376103a052be43747b6ae46de0
nithish95 <nithish1995@gmail.com>
2016-04-06 21:17:45 +0530

    Create dict.py
    Program using python dictionaries.They work like associative arrays.

---
+++ dict.py
@@ -0,0 +1,9 @@
+dict={}
+dict[2]="THIS IS TWO"
+dict['one']="THIS is ONE"
+tinydict={'name':"hari",'code':574154,'dept':'sales'}
+print dict[2]
+print dict['one']
+print tinydict
+print tinydict.keys()
+print tinydict.values()
